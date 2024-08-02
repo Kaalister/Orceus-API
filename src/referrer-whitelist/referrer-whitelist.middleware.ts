69cd12a80ed6ed4e93d54b6262ba7ac614b0a14e
Samuel Caillot <samuel.c@boaton.fr>
2024-08-02 11:48:26 +0200

    change security

--- src/referrer-whitelist/referrer-whitelist.middleware.ts
+++ src/referrer-whitelist/referrer-whitelist.middleware.ts
@@ -3,19 +3,22 @@ import { Request, Response, NextFunction } from 'express';
 
 @Injectable()
 export class ReferrerWhitelistMiddleware implements NestMiddleware {
-	private allowedReferrers: string[] = [
-		'https://orceus.fr',
-		'https://www.orceus.fr'
-  	]
+	private allowedReferrerPatterns: RegExp[] = [
+		/^https:\/\/www\.orceus\.fr\//,
+		/^https?:\/\/localhost:3000?\//
+	]
 
-  	use(req: Request, res: Response, next: NextFunction) {
-		const referrer = req.headers.referer || req.headers.referrer
-		console.log('referrer', referrer)
-		if (this.allowedReferrers.includes(referrer as string)) {
-	  		next()
+	use(req: Request, res: Response, next: NextFunction) {
+		const referrer = (req.headers.referer || req.headers.referrer) as string | undefined;
+		console.log('referrer', referrer);
+
+		if (referrer && this.allowedReferrerPatterns.some(pattern =>
+			pattern.test(referrer)
+		)) {
+			next();
 		} else {
-			console.error('Access denied')
-			res.status(403).send('Access denied')
+			console.error('Access denied');
+			res.status(403).send('Access denied');
 		}
-  	}
+	}
 }
