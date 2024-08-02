6aaa12f5e19f36428ac617a39f7945d6663cc44d
Samuel Caillot <samuel.c@boaton.fr>
2024-08-02 13:05:06 +0200

    add security by ip too

--- src/referrer-whitelist/referrer-whitelist.middleware.ts
+++ src/referrer-whitelist/referrer-whitelist.middleware.ts
@@ -5,20 +5,31 @@ import { Request, Response, NextFunction } from 'express';
 export class ReferrerWhitelistMiddleware implements NestMiddleware {
 	private allowedReferrerPatterns: RegExp[] = [
 		/^https:\/\/www\.orceus\.fr\//,
-		/^https?:\/\/localhost:3000?\//
+		/^https?:\/\/localhost:3000\//,
 	]
-
+  
+	private allowedIPs: string[] = [
+		'84.100.220.90',
+	]
+  
 	use(req: Request, res: Response, next: NextFunction) {
-		const referrer = (req.headers.referer || req.headers.referrer) as string | undefined;
-		console.log('referrer', referrer);
-
-		if (referrer && this.allowedReferrerPatterns.some(pattern =>
+		const referrer = (
+			req.headers.referer || req.headers.referrer
+		) as string | undefined
+		const ip = req.ip || req.connection.remoteAddress;
+  
+	  console.log('referrer', referrer)
+	  console.log('ip', ip)
+  
+	  if (
+		(referrer && this.allowedReferrerPatterns.some(pattern =>
 			pattern.test(referrer)
-		)) {
-			next();
-		} else {
-			console.error('Access denied');
-			res.status(403).send('Access denied');
-		}
+		)) || (ip && this.allowedIPs.includes(ip))
+	  ) {
+		next();
+	  } else {
+		console.error('Access denied');
+		res.status(403).send('Access denied');
+	  }
 	}
 }
